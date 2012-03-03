28860949510fb7ccc972c2c55508d37469b5187a
Will Cosgrove <will@willcosgrove.com>
2012-03-03 03:42:56 -0600

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MusicAnalyzer::Application.config.secret_token = '3c4b4c4bc6e1519456f66dc1efc2677392c74736fc6699e7ea77abd8c8edfb9b40486778ac1c90d56f6b38be348048648fad3f9dbdd5f63e87fc46cd2cfcf5fd'
